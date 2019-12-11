756e4125581ef5b57241ad9cf1880d19935131be
Kuba K <kuba5k5@gmail.com>
2019-12-11 01:46:56 +0100

    Setting routes and aws settings

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "bucket-for-note-upload"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://pojczycv2k.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_uWV8LeDmI",
+      APP_CLIENT_ID: "677efo82pf6r36r6i6ks98ord5",
+      IDENTITY_POOL_ID: "us-east-2:c20df05b-47d4-422f-8b22-989431d48e94"
+    }
+  };
+  
